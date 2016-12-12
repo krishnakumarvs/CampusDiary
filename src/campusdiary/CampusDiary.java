2c885ca7e18765442e1027a76fde63f2fcbef8a8
krishnakumarvs <krishh_mea@yahoo.in>
2016-12-12 09:48:24 +0530

    Initial files

---
+++ src/campusdiary/CampusDiary.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package campusdiary;
+
+/**
+ *
+ * @author user
+ */
+public class CampusDiary {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Login s = new Login();
+        s.setVisible(true);
+    }
+}
