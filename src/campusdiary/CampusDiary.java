f2d9323b72c7b3dcb4f612575bf2658d9f37e9e1
amrutharagdivya <americanbluerose@gmail.com>
2017-02-19 16:01:05 +0530

    database changes

--- src/campusdiary/CampusDiary.java
+++ src/campusdiary/CampusDiary.java
@@ -14,7 +14,7 @@ public class CampusDiary {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Login s = new Login();
+        CampusStart s = new CampusStart();
         s.setVisible(true);
     }
 }
